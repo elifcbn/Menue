49f936149509a9d7558b2562cb57f9e34d65f187
Elif Cobansoy <inf2065@hs-worms.de>
2018-11-13 11:14:36 +0100

    Initial commit

---
+++ app/src/main/java/com/example/elifc/menue/Fragment_2.java
@@ -0,0 +1,21 @@
+package com.example.elifc.menue;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment_2 extends Fragment {
+
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.layout_2, container, false);
+        return view;
+    }
+}
